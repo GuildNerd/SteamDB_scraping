b6ac0824a0c075cab967b72138a3ebfdea59fffe
Artur Oliveira <artur.oliveiraprado@gmail.com>
2023-06-05 11:07:18 -0300

    Foi Adicionado dois arquivos, um para links já visitados,title.txt, e um para links ainda não visitados,links.txt.

--- insert_games_in_DB.py
+++ insert_games_in_DB.py
@@ -2,15 +2,22 @@ import sqlite3
 from selenium.webdriver.firefox.service import Service
 import time
 from datetime import datetime
-from steamDB import drive_iniciate 
+import steamDB
 from bs4 import BeautifulSoup
 
 
 plataform = 'Pc'
-conn = sqlite3.connect('documents_html.db')
+conn = sqlite3.connect('steamDB.db')
 c = conn.cursor()
-c.execute('''CREATE TABLE IF NOT EXISTS documents_html
+c.execute('''CREATE TABLE IF NOT EXISTS steamDB
              (id INTEGER PRIMARY KEY, url TEXT, html TEXT, plataform TEXT, timestamp DATETIME)''')
+
+
+def get_file_length():
+    with open('links.txt', 'r') as file:
+        line = file.readlines()
+        length = line[-1]
+    return length
 
 
 def get_current_time():
@@ -18,35 +25,57 @@ def get_current_time():
     current_time = now.strftime("%Y-%m-%d %H:%M:%S")
     return current_time
 
-i=0
-j=0
-lista =[]
-with open('fe.txt','r') as file:
-    while (i<1878):
-        lista.append(file.readline())
-        i+=1
-       
- 
-#to drible the bot searcher need to open a browser to each one link we have (1878 links)
-while j< len(lista):
-    #get the Driver to navegate to all links
-    driver = drive_iniciate()
-    print(lista[j])
-    #go to an site of the list
-    page = driver.get(lista[j])
-    #get all html
-    html = driver.page_source
-    #parse the html
-    soup = BeautifulSoup(html,'html.parser')
-    #get the body in the html
-    bod = soup.find_all('body','en_US')
-    body = str(bod)
 
-    time.sleep(3)
-    #save the informations in a DB called documents
-    c.execute("INSERT INTO documents_html (url, html, plataform, timestamp ) VALUES ( ?, ?, ?, ?)", (lista[j], body, plataform, get_current_time()))
-    conn.commit()
-    driver.close()
-    j+=1
+def start():
+    i=0
+    j=0
+    lista =[]
+    #iniciate the scrapping
+    #steamDB.initiate()
+    length = get_file_length()
+    print(length)
+    #open the file
+    with open('links.txt','r') as file:
+        while (i<int(length)):
+            lista.append(file.readline())
+            i+=1
+    #to drible the bot searcher need to open a browser to each one link we have (1878 links)
+    while j< len(lista):
+        #get the Driver to navegate to all links
+        driver = steamDB.drive_initiate()
+        print(lista[j])
+        #go to an site of the list
+        page = driver.get(lista[j])
+        #get all html
+        html = driver.page_source
+        #parse the html
+        soup = BeautifulSoup(html,'html.parser')
+        #get the url in the html
+        url = soup.find_all("nav", "app-links")
+        url = str(url)
+        soups = BeautifulSoup(url,'html.parser')
+        url = soups.find("a")
+        #get the tile in the html
+        title = soup.find("h1")
 
-conn.close()
+        #get the price in the html
+        price = soup.find("tr","table-prices-current")
+        price = str(price)
+        soupo = BeautifulSoup(price,'html.parser')
+        price = soupo.find_all('td')
+        soupo = str(soupo)
+        info = ''
+        info += str(title)
+        if(soupo != 'None'):
+            info += str(price[1])
+        time.sleep(1)
+        #save the informations in a DB called steamDB
+        c.execute("INSERT INTO steamDB (url, html, plataform, timestamp ) VALUES ( ?, ?, ?, ?)", (url.get('href'), info, plataform, get_current_time()))
+        conn.commit()
+        driver.close()
+        j+=1
+        print(j)
+        print(' ')
+        time.sleep(6)
+    conn.close()
+start()
--- steamDB.py
+++ steamDB.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
-import sqlite3
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import time
 
-
+lines=0
 #conn = sqlite3.connect('documentis_html.db')
 #c = conn.cursor()
 #c.execute('''CREATE TABLE IF NOT EXISTS documentis_html
@@ -11,7 +12,13 @@ import time
 
 # initiate the drive to navigate on web
 def drive_initiate():
-    driver = webdriver.Chrome()
+    FFdriver = '/snap/firefox/2667/usr/lib/firefox/geckodriver'
+    firefox = '/snap/firefox/2667/usr/lib/firefox/firefox'
+
+
+    service = Service(executable_path = FFdriver)
+    # Set up the Selenium webdriver
+    driver = webdriver.Firefox(service = service, firefox_binary = firefox)
     return driver
 
 
@@ -30,30 +37,57 @@ def get_child_pages(html):
     # Use BeautifulSoup to parse the HTML
     soup = BeautifulSoup(html, 'html.parser')
     # listed games
-    infos = soup.find_all("td>a")
+    data = soup.find_all("td","applogo text-left")
+    data = str(data)
+    soup= BeautifulSoup(data,'html.parser')
+    infos = soup.find_all('a') 
     links = []
     # append all the links of the game pages
     for info in infos:
+        print(info)
         gamePageUrl = "https://steamdb.info"+info.get("href")
         gamePageUrl = gamePageUrl[0:-7]
         links.append(str(gamePageUrl))
     return links
 
+def next_page(html, driver):
+    i= 0
+    while i<62:
+        childpages = get_child_pages(html)
+        write(childpages)
+        soup = BeautifulSoup(html, 'html.parser')
+        time.sleep(1)
+        button = driver.find_element(By.ID,"table-apps_next")
+        button.click()
+        i+=1
+
+#write all links in a .TXT
+def write(list):
+    count_lines = 0
+    with open('links.txt', 'a') as f:
+        for links in list:
+            link = str(links)
+            f.write(f'{links}\n')
+            count_lines+=1
+        global lines
+        lines+=count_lines
+
+def write_number_of_lines():
+    with open('links.txt', 'a') as f:
+        global lines
+
+        f.write(str(lines))
 
 # main function
 def initiate():
     driver = drive_initiate()
     rootpage = get_root_page(driver)
-    childpages = get_child_pages(rootpage)
-
-    i = 0
-    while (i<5):
-        time.sleep(2)
-        print(childpages[i])
+    next_page(rootpage, driver)
+    write_number_of_lines()
 
     driver.quit()
     
-initiate()
+
 
 # Close the Selenium webdriver
 
